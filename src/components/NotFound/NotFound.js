adc31db92aa03df97715480fb0533a72242da590
Tareq Hassan <tareqhassan2014@gmail.com>
2021-03-11 18:32:01 +0600

    404 page stuling

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import oops from './opps.png';
+import { useHistory } from 'react-router';
+
 
 const NotFound = () => {
+
+    const history = useHistory();
+
     return (
-        <div>
-            <p>404 Not Found! </p>
+        <div className='text-center'>
+            <img src={oops} alt=""/>
+            <h2>404-PAGE NOT FOUND</h2>
+            <p>The page you are looking for might removed</p>
+            <p>had it's name changed or it temporary unavailable</p>
+            <button className='btn btn-primary' onClick={() => history.push(`/`)}>GO TO HOMEPAGE</button>
         </div>
     );
 };
