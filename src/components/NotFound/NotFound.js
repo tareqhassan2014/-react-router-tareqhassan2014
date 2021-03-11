6ecfaa1c46793cc13ee63b0a4d7588bb7bbcef12
Tareq Hassan <tareqhassan2014@gmail.com>
2021-03-11 18:37:14 +0600

    finished

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -8,11 +8,10 @@ const NotFound = () => {
     const history = useHistory();
 
     return (
-        <div className='text-center'>
+        <div className='text-center pt-5'>
             <img src={oops} alt=""/>
             <h2>404-PAGE NOT FOUND</h2>
-            <p>The page you are looking for might removed</p>
-            <p>had it's name changed or it temporary unavailable</p>
+            <p>The page you are looking for might removed <br/> had it's name changed or it temporary unavailable</p>
             <button className='btn btn-primary' onClick={() => history.push(`/`)}>GO TO HOMEPAGE</button>
         </div>
     );
